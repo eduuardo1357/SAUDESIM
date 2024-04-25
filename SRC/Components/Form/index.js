30698c512d8440f6097c4d1b426eedbab5188ad4
eduuardo1357 <eduardo.silva1077@etec.sp.gov.br>
2024-04-25 08:37:48 -0300

    adicionar Button e useState

--- SRC/Components/Form/index.js
+++ SRC/Components/Form/index.js
@@ -1,19 +1,31 @@
-import React from "react";
-import { View, TextInput, Text} from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Text, Button} from "react-native";
 
 export default function Form(){
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageIMC, setMessageIMC] = useState("preencha o peso e altura");
+    const [imc, setIMC] = useState(null)
+    const [textButton, setTextButton] = useState("IMC")
+
+
     return( 
         <View>
            <Text>Altura</Text>
            <TextInput
-           placeholder="Ex. 1.75"
+           placeholder="Ex. 1.69"
            keyboardType="numeric"
            />
            <Text>Peso</Text>
            <TextInput
-           placeholder="Ex. 71.5"
-           keyboardType="numeric"
-           />
+           placeholder="Ex. 48.9"
+           keyboardType="numeric"/>
+
+        <Button
+        onePress={() => validationImc ()}
+        title={textButton}
+        />  
+
         </View>
     );
 }
