fb016519e26d43201953a5126d6a3be197caf66e
eduuardo1357 <eduardo.silva1077@etec.sp.gov.br>
2024-06-13 08:48:40 -0300

    estilização do titulo

--- SRC/Components/Title/Index.js
+++ SRC/Components/Title/Index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text} from "react-native";
+import styles from "./style";
 
 export default function Title(){
     return( 
-        <View>
-           <Text> SAUDESIM</Text>
+        <View style={styles.BoxTitle}>
+           <Text style={styles.textTitle}> SAUDESIM</Text>
         </View>
     );
 }
---
+++ SRC/Components/Title/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    BoxTitle: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        
+    },
+
+    textTitle: {
+        color: "#91BB77",
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+
+});
+export default styles
